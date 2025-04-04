5324f3b7123e5738d541cd5d5542bd7866e84506
PaulTheCheese <psangabriel52@gmail.com>
2025-04-05 01:42:16 +0800

    Delete medic-website-project directory

--- medic-website-project/client/src/components/NavBar/DynamicNavBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useAuth } from '../context/AuthContext';
-import { NavBar } from './NavBar';
-import { AdminNavBar } from './AdminNavBar';
-
-export const DynamicNavBar = () => {
-  const { user } = useAuth();
-  return user?.isAdmin ? <AdminNavBar /> : <NavBar />;
-};
